6adeb014744d541a3e6a93bcf944189b9ef6ca47
Luukuton <mleinikka@gmail.com>
2021-02-04 19:50:25 +0200

    Testing image generation

--- src/test/java/ryuuri/dao/ImageUtilTest.java
+++ src/test/java/ryuuri/dao/ImageUtilTest.java
@@ -1,25 +1,41 @@
 package ryuuri.dao;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImageUtilTest {
     private ImageUtil imageUtil;
+    int[][] data;
 
     @BeforeEach
     void setUp() {
-        int[][] data = {{1,1,1},{1,0,0},{0,0,1}};
+        // 3x3, 25%, 0 steps, seed: -8653063932943180940
+        data = new int[][] {{1, 1, 1}, {0, 0, 0}, {1, 0, 1}};
         imageUtil = new ImageUtil(data);
     }
 
     @Test
-    void generateImage() {
+    @Tag("UnitTest")
+    void generateImage() throws IOException {
         WritableImage output = imageUtil.generateImage();
-        // TODO: expected from file
+        InputStream resourceBuff= Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("dungeon@1.png"));
+        Image image = SwingFXUtils.toFXImage(ImageIO.read(resourceBuff), null);
+        for (int x = 0; x < data.length; x++) {
+            for (int y = 0; y < data[x].length; y++) {
+                assertTrue((image.getPixelReader().getArgb(x, y) == output.getPixelReader().getArgb(x, y)));
+            }
+        }
     }
 
     @Test
